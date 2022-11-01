fcc171252ccbdc5553beb324ed262ef30812bb4e
victor-neutzling <victorneutzling2001@gmail.com>
2022-11-01 14:21:46 -0300

    adding router and a few routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Register from './pages/Register';
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  useNavigate,
+} from "react-router-dom";
+import Home from './pages/Home';
 
 function App() {
+
+ 
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />
+    },
+    {
+      path: '/register',
+      element: <Register />
+    }
+
+  ])
+
   return (
     <div className="App">
-      <Header />
-      <Register />
+      <Header/>
+      <RouterProvider router={router} />
     </div>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
+
 import Button from '../Button'
 import styles from './Header.module.scss'
 
 export default function Header() {
+
   return (
     <div className={styles.header}>
         <div className={styles.logo}>
-            <h1>plebbit</h1>
+            <h1 onClick={() => window.location.href='/'}>plebbit</h1>
         </div>
         <div className={styles.links}>
           <div className={styles.buttonWrapper}>
               <Button>login</Button>
           </div>
           <div className={styles.buttonWrapper}>
-              <Button>register</Button>
+              <Button onClick={() => window.location.href='/register'}>register</Button>
           </div>
         </div>
     </div>
